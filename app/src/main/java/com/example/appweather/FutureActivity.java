9880286c64729af6059978137b6c81a711d640e3
tranhoangtrieu <tranhoangtrieu03092003@gmail.com>
2024-01-07 00:26:28 +0700

    new

---
+++ app/src/main/java/com/example/appweather/FutureActivity.java
@@ -0,0 +1,14 @@
+package com.example.appweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FutureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_future);
+    }
+}

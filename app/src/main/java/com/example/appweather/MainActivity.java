e718ef05047a3c0592bdf2e4320e3bd05d843985
ldThanhTrung <ldthanhtrung.2106@gmail.com>
2024-01-07 13:03:08 +0700

    new

--- app/src/main/java/com/example/appweather/MainActivity.java
+++ app/src/main/java/com/example/appweather/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.appweather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -13,17 +15,29 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     static final String API_KEY = "d15dcebccc91478483885fba781fc2a6" ;
+    TextView txtTemperState;
+    ImageView imgWeatherIcon;
+    TextView Text_datetime;
+    TextView Text_temperature;
+    TextView Text_Humidity;
+    TextView Text_Wind_Speed;
+    TextView Text_cloud;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getJsonWeather("hanoi");
     }
 
     public void getJsonWeather(String city) {
@@ -38,11 +52,26 @@ public class MainActivity extends AppCompatActivity {
                             JSONObject weatherObj = weatherArray.getJSONObject(0);
                             String icon = weatherObj.getString("icon");
                             String urlIcon = "https://openweathermap.org/img/wn/" + icon + ".png";
+                            Picasso.get().load(urlIcon).into(imgWeatherIcon);
+                            String temperState = weatherObj.getString("description");
+                            txtTemperState.setText(temperState);
                             JSONObject main = response.getJSONObject("main");
                             String temp = response.getString("temp");
+                            Text_temperature.setText(temp+"°");
                             String humidity = main.getString("humidity");
+                            Text_Humidity.setText(humidity+"%");
                             JSONObject wind = response.getJSONObject("wind");
                             String speed = wind.getString("wind");
+                            Text_Wind_Speed.setText(speed+"m/s");
+                            JSONObject clouds = response.getJSONObject("clouds");
+                            String all = clouds.getString("all");
+                            Text_cloud.setText(all + "%");
+                            String date = response.getString("dt");
+                            long lDate = Long.parseLong(date);
+                            SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE, dd-MM-yyyy HH:mm:ss");
+                            Date dates = new Date(lDate);
+                            String currenttime = dateFormat.format(dates);
+                            Text_datetime.setText(currenttime);
                         } catch (JSONException e) {
                             e.printStackTrace();
                             throw new RuntimeException(e);
